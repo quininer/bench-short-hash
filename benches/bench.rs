a9b3c148f617480dd94fc56079c25c5ae924aa1c
quininer kel <quininer@live.com>
2017-05-16 22:21:36 +0800

    feat: bench bytes

--- benches/bench.rs
+++ benches/bench.rs
@@ -18,10 +18,24 @@ extern crate jhash;
 use test::{ Bencher, black_box };
 
 lazy_static!{
-    static ref INPUT: [u8; 128] = {
+    static ref INPUT16: [u8; 16] = {
+        use rand::{ Rng, thread_rng };
+
+        let mut input = [0; 16];
+        thread_rng().fill_bytes(&mut input);
+        input
+    };
+    static ref INPUT128: [u8; 128] = {
         use rand::{ Rng, thread_rng };
 
         let mut input = [0; 128];
+        thread_rng().fill_bytes(&mut input);
+        input
+    };
+    static ref INPUT1024: [u8; 1024] = {
+        use rand::{ Rng, thread_rng };
+
+        let mut input = [0; 1024];
         thread_rng().fill_bytes(&mut input);
         input
     };
@@ -30,40 +44,75 @@ lazy_static!{
 
 macro_rules! bench_hasher {
     ( : $name:ident, ( $( $p:path );* ), $expr:expr ) => {
-        #[bench]
-        fn $name(b: &mut Bencher) {
+        mod $name {
+            use ::*;
             $(
                 use $p;
             )*
 
-            b.iter(|| $expr);
+            #[bench] fn input_16(b: &mut Bencher) {
+                b.bytes = INPUT16.len() as _;
+                b.iter(|| ($expr)(&*INPUT16));
+            }
+            #[bench] fn input_128(b: &mut Bencher) {
+                b.bytes = INPUT128.len() as _;
+                b.iter(|| ($expr)(&*INPUT128));
+            }
+            #[bench] fn input_1024(b: &mut Bencher) {
+                b.bytes = INPUT1024.len() as _;
+                b.iter(|| ($expr)(&*INPUT1024));
+            }
         }
     };
     ( $name:ident, ( $( $p:path );* ), $default:expr ) => {
-        #[bench]
-        fn $name(b: &mut Bencher) {
+        mod $name {
+            use ::*;
             use std::hash::Hasher;
             $(
                 use $p;
             )*
 
-            b.iter(|| {
-                let mut hasher = $default;
-                black_box(&mut hasher).write(black_box(&*INPUT));
-                black_box(hasher).finish();
-            });
+            #[bench]
+            fn input_16(b: &mut Bencher) {
+                b.bytes = INPUT16.len() as _;
+                b.iter(|| {
+                    let mut hasher = $default;
+                    black_box(&mut hasher).write(black_box(&*INPUT16));
+                    black_box(hasher).finish();
+                });
+            }
+
+            #[bench]
+            fn input_128(b: &mut Bencher) {
+                b.bytes = INPUT128.len() as _;
+                b.iter(|| {
+                    let mut hasher = $default;
+                    black_box(&mut hasher).write(black_box(&*INPUT128));
+                    black_box(hasher).finish();
+                });
+            }
+
+            #[bench]
+            fn input_1024(b: &mut Bencher) {
+                b.bytes = INPUT1024.len() as _;
+                b.iter(|| {
+                    let mut hasher = $default;
+                    black_box(&mut hasher).write(black_box(&*INPUT1024));
+                    black_box(hasher).finish();
+                });
+            }
         }
     };
 }
 
-bench_hasher!(: bench_crc32, (crc::crc32; crc::Hasher32), {
+bench_hasher!(: bench_crc32, (crc::crc32; crc::Hasher32), |input| {
     let mut hasher = crc32::Digest::new(crc32::IEEE);
-    black_box(&mut hasher).write(black_box(&*INPUT));
+    black_box(&mut hasher).write(black_box(input));
     black_box(hasher).sum32();
 });
-bench_hasher!(: bench_crc64, (crc::crc64; crc::Hasher64), {
+bench_hasher!(: bench_crc64, (crc::crc64; crc::Hasher64), |input| {
     let mut hasher = crc64::Digest::new(crc64::ECMA);
-    black_box(&mut hasher).write(black_box(&*INPUT));
+    black_box(&mut hasher).write(black_box(input));
     black_box(hasher).sum64();
 });
 
@@ -71,23 +120,23 @@ bench_hasher!(bench_xxhash32, (twox_hash::XxHash32), XxHash32::default());
 bench_hasher!(bench_xxhash64, (twox_hash::XxHash), XxHash::default());
 
 bench_hasher!(bench_siphasher, (siphasher::sip::SipHasher), SipHasher::new());
-bench_hasher!(: bench_siphasher128, (std::hash::Hasher; siphasher::sip128::SipHasher; siphasher::sip128::Hasher128), {
+bench_hasher!(: bench_siphasher128, (std::hash::Hasher; siphasher::sip128::SipHasher; siphasher::sip128::Hasher128), |input| {
     let mut hasher = SipHasher::new();
-    black_box(&mut hasher).write(black_box(&*INPUT));
+    black_box(&mut hasher).write(black_box(input));
     black_box(hasher).finish128();
 });
 
 bench_hasher!(bench_farmhash, (farmhash::FarmHasher), FarmHasher::default());
-bench_hasher!(: bench_farmhash32, (farmhash::fingerprint32), fingerprint32(black_box(&*INPUT)));
-bench_hasher!(: bench_farmhash64, (farmhash::fingerprint64), fingerprint64(black_box(&*INPUT)));
+bench_hasher!(: bench_farmhash32, (farmhash::fingerprint32), |input| fingerprint32(black_box(input)));
+bench_hasher!(: bench_farmhash64, (farmhash::fingerprint64), |input| fingerprint64(black_box(input)));
 
 bench_hasher!(bench_metrohash64, (metrohash::MetroHash64), MetroHash64::default());
 bench_hasher!(bench_metrohash128, (metrohash::MetroHash128), MetroHash128::default());
 
-bench_hasher!(: bench_murmurhash3_32, (murmurhash3::murmurhash3_x86_32), murmurhash3_x86_32(black_box(&*INPUT), 42));
-bench_hasher!(: bench_murmurhash3_128, (murmurhash3::murmurhash3_x64_128), murmurhash3_x64_128(black_box(&*INPUT), 42));
+bench_hasher!(: bench_murmurhash3_32, (murmurhash3::murmurhash3_x86_32), |input| murmurhash3_x86_32(black_box(input), 42));
+bench_hasher!(: bench_murmurhash3_128, (murmurhash3::murmurhash3_x64_128), |input| murmurhash3_x64_128(black_box(input), 42));
 
-bench_hasher!(: bench_djb33, (djb33::djb33), djb33(42, black_box(&*INPUT)));
+bench_hasher!(: bench_djb33, (djb33::djb33), |input| djb33(42, black_box(input)));
 bench_hasher!(bench_fnv, (fnv::FnvHasher), FnvHasher::default());
 bench_hasher!(bench_seahash, (seahash::SeaHasher), SeaHasher::new());
 bench_hasher!(bench_jhash, (jhash::JHasher), JHasher::default());
